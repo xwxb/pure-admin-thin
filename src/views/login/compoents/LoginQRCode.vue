c4be7fca8df8d4a4fa8531257401c33e71d45c3f
xban <x_ban@qq.com>
2024-06-17 00:28:53 +0800

    fix: 1

--- src/views/login/compoents/LoginQRCode.vue
+++ src/views/login/compoents/LoginQRCode.vue
@@ -1,43 +1,42 @@
 <script setup lang="ts">
 import Motion from "../utils/motion";
 import ReQrcode from "@/components/ReQrcode";
-import { useUserStoreHook } from "@/store/modules/user";
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 
-const api = "https://47.94.10.223:12301/auth/qrcode/uuid";
-const qrCodeData = ref("");
+// const api = "https://47.94.10.223:12301/auth/qrcode/uuid";
+const qrCodeData = ref("admin");
 
-const refreshQRCode = () => {
-  fetch(api)
-    .then(res => res.json())
-    .then(data => {
-      qrCodeData.value = data.uuid;
-      console.log(data.uuid);
-    });
-};
+// const refreshQRCode = () => {
+//   fetch(api)
+//     .then(res => res.json())
+//     .then(data => {
+//       qrCodeData.value = data.uuid;
+//       console.log(data.uuid);
+//     });
+// };
 
-onMounted(() => {
-  refreshQRCode(); // 初始加载二维码数据
+// onMounted(() => {
+//   refreshQRCode(); // 初始加载二维码数据
 
-  setInterval(() => {
-    refreshQRCode(); // 每20秒刷新二维码数据
-  }, 20000);
-});
+//   setInterval(() => {
+//     refreshQRCode(); // 每20秒刷新二维码数据
+//   }, 20000);
+// });
 
-const checkAPI = "https://47.94.10.223:12301/auth/qrcode/status";
-// 每三秒检查一次，如果返回结果是 data.message == "success" 则登录成功
-setInterval(() => {
-  fetch(checkAPI)
-    .then(res => res.json())
-    .then(data => {
-      if (data.message === "success") {
-        useUserStoreHook().loginByUsername({
-          username: "admin",
-          password: "admin123"
-        });
-      }
-    });
-}, 3000);
+// const checkAPI = "https://47.94.10.223:12301/auth/qrcode/status";
+// // 每三秒检查一次，如果返回结果是 data.message == "success" 则登录成功
+// setInterval(() => {
+//   fetch(checkAPI)
+//     .then(res => res.json())
+//     .then(data => {
+//       if (data.message === "success") {
+//         useUserStoreHook().loginByUsername({
+//           username: "admin",
+//           password: "admin123"
+//         });
+//       }
+//     });
+// }, 3000);
 </script>
 
 <template>
